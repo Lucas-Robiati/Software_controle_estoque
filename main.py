3bd3a7b0e8b14867370ea3f8b31e5cd9f1e43e3c
Lucas-Robiati <lucas.santana.280304@gmail.com>
2025-07-04 09:21:55 -0300

    Pasando para orientado a objeto

--- main.py
+++ main.py
@@ -11,38 +11,47 @@ DBUSER = os.getenv('DBUSER', 'postgres')
 DBPASSWORD = os.getenv('DBPASSWORD', 'postgres')
 DBHOST = os.getenv('DBHOST', 'localhost')
 
-def get_db_connection():
-    return psycopg2.connect(
-        dbname=DBNAME,
-        user=DBUSER,
-        password=DBPASSWORD,
-        host=DBHOST
-    )
+class Database_conect:
+    def __init__(self):
+        self.cur = None
+        self.conn = None
+        self.create_database()
+        self.create_tables()
 
-conn = get_db_connection()
+    def get_db_connection(self):
+        return psycopg2.connect(
+            dbname=DBNAME,
+            user=DBUSER,
+            password=DBPASSWORD,
+            host=DBHOST
+        )
 
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) 
-cur = conn.cursor()
+    def create_database(self):
+        self.conn = self.get_db_connection()
+        self.conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) 
+        self.cur = self.conn.cursor()
 
-DBNAME = "estoque"
-sql = (f"SELECT 'CREATE DATABASE {DBNAME}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '{DBNAME}')")
+        DBNAME = "estoque"
+        sql = (f"SELECT 'CREATE DATABASE {DBNAME}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '{DBNAME}')")
 
-try:
-    cur.execute(sql)
-except:
-    print("passou")
-    cur.execute(sql.SQL('CREATE DATABASE {}').format(sql.Identifier(DBNAME)))
+        try:
+            self.cur.execute(sql)
+        except:
+            self.cur.execute(sql.SQL('CREATE DATABASE {}').format(sql.Identifier(DBNAME)))
 
-cur.close()
-conn.close()
+        self.cur.close()
+        self.conn.close()
 
-conn = get_db_connection()
-cur  = conn.cursor()
+    def create_tables(self):
+        self.conn = self.get_db_connection()
+        self.cur  = self.conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS produtos " \
-"(id integer NOT NULL, nome varchar(60) NOT NULL, quantidade_es int NOT NULL, preco float NOT NULL)")
-conn.commit()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS produtos " \
+        "(id integer NOT NULL, nome varchar(60) NOT NULL, quantidade_es int NOT NULL, preco float NOT NULL)")
+        self.conn.commit()
 
-cur.close()
-conn.close()
+        self.cur.close()
+        self.conn.close()
+
+db = Database_conect()
 #result = cur.fetchone()[0]
