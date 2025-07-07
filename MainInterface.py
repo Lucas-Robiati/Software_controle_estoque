7d7ca4a544d84fb1eaa52709a32b79dfcd635586
Arnaldo Jr <arnaldo.junior2@estudante.ifms.edu.br>
2025-07-07 11:34:30 -0400

    Inserção de sidebar

--- MainInterface.py
+++ MainInterface.py
@@ -12,17 +12,39 @@ class Application(Validate):
 
   def window(self):
   # Funcao da janela principal
-    self.root.title("InerCalc")                                  # Nome do software
-    self.root.configure(background= Color.dark_blue.value)       # Background da janela principal
+    self.root.title("Controle de Estoque")                       # Nome do software
+    self.root.configure(background= Color.white.value)           # Background da janela principal
     self.root.geometry("950x520")                                # Geometria da janela 
     self.root.minsize(width=950, height=520)                     # Tamanho minimo da janela 
     self.root.resizable(True,True)                               # Software pode redimensionar 
     self.root.protocol('WM_DELETE_WINDOW', self.destroy_window)
+    self.layout_sidebar()
 
-  
+  def layout_sidebar(self):
+    self.sidebar = Frame(self.root, width=1000, bg=Color.light_blue.value)
+    self.sidebar.pack(side="left", fill="y")
+
+    btn_produtos = self.create_button(self.sidebar, "Produtos", command=None)
+    btn_entrada = self.create_button(self.sidebar, "Entradas", command=None)
+    btn_saida = self.create_button(self.sidebar, "Saidas", command=None)
+    btn_cliente = self.create_button(self.sidebar, "Clientes", command=None)
+    
+  def create_button(self, frame, text, command):  
+    btn = Button(frame, 
+                 text=text,
+                 command=command, 
+                 bg=Color.light_blue.value, 
+                 fg=Color.white.value, 
+                 borderwidth=0, 
+                 highlightthickness= 0, 
+                 relief="flat")
+    btn.pack(fill="x", pady=5, padx=10)
+
+    return btn
+    
+
   def destroy_window(self):
     root.destroy()        # Destroi a janela do Tkinter
-
 
 root = Tk()
 Application(root)
